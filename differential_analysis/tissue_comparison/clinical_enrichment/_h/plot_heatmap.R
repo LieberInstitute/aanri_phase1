15644725af68957fc08d815144fc8d6379714f41
KrotosBenjamin <kj.benjamin90@gmail.com>
2021-10-13 12:19:47 -0400

    Updating enrichment heatmp scaling for odds ratio

--- differential_analysis/tissue_comparison/clinical_enrichment/_h/plot_heatmap.R
+++ differential_analysis/tissue_comparison/clinical_enrichment/_h/plot_heatmap.R
@@ -12,21 +12,32 @@ load_enrichment <- function(){
 }
 memENRICH <- memoise::memoise(load_enrichment)
 
-plot_tile <- function(label){
-    tile_plot <- memENRICH() %>% mutate_if(is.character, as.factor) %>%
+gen_data <- function(){
+    err = 0.0000001
+    dt <- memENRICH() %>% mutate_if(is.character, as.factor) %>%
         filter(str_detect(Comparison, label)) %>%
         mutate(Tissue=fct_relevel(Tissue, rev), `-log10(FDR)`= -log10(FDR),
                `OR Percentile`= OR / (1+OR), p.fdr.sig=FDR < 0.05,
+               `log2(OR)` = log10(OR+err),
                p.fdr.cat=cut(FDR, breaks=c(1,0.05,0.01,0.005,0),
                              labels=c("<= 0.005","<= 0.01","<= 0.05","> 0.05"),
-                             include.lowest=TRUE)) %>%
-        ggplot(aes(x = Comparison, y = Tissue, fill = `OR Percentile`,
+                             include.lowest=TRUE))
+    return(dt)
+}
+memDF <- memoise::memoise(gen_data)
+
+plot_tile <- function(label){
+    y0 <- min(memDF()$`log2(OR)`)-0.1
+    y1 <- max(memDF()$`log2(OR)`)+0.1
+    tile_plot <- memDF() %>%
+        ggplot(aes(x = Comparison, y = Tissue, fill = `log2(OR)`,
                    label = ifelse(p.fdr.sig,
                                   format(round(`-log10(FDR)`,1), nsmall=1), ""))) +
         ylab('Ancestry-Related DEGs') + xlab('') +
         geom_tile(color = "grey") + ggfittext::geom_fit_text(contrast = TRUE) +
-        scale_fill_gradient2(midpoint = 0.5, low = "blue", mid = "white",
-                             high = "red", space = "Lab", limits=c(0,1)) +
+        scale_fill_gradientn(colors=c("blue", "white", "red"),
+                             values=scales::rescale(c(y0, 0, y1)),
+                             limits=c(y0,y1)) +
         ggpubr::theme_pubr(base_size = 20, border=TRUE) +
         theme(axis.text.x = element_text(angle = 45, hjust=1),
               legend.position="right",
