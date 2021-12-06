8c10dc328b9f13848ea21cbdf7b6a1da0d264e53
KrotosBenjamin <kj.benjamin90@gmail.com>
2021-12-06 11:20:24 -0500

    updating scripts for clincial enrichment; adding parkinsons, alzheimers, and lieber bipolar

--- differential_analysis/tissue_comparison/clinical_enrichment/_h/plot_heatmap.R
+++ differential_analysis/tissue_comparison/clinical_enrichment/_h/plot_heatmap.R
@@ -7,15 +7,20 @@ save_plot <- function(p, fn, w, h){
     }
 }
 
-load_enrichment <- function(){
-    return(data.table::fread("clincial_phenotypes_enrichment_analysis.tsv"))
+load_DEG_enrichment <- function(){
+    return(data.table::fread("clincial_phenotypes_enrichment_analysis_DEGs.tsv"))
 }
-memENRICH <- memoise::memoise(load_enrichment)
+memDEG <- memoise::memoise(load_DEG_enrichment)
 
-gen_data <- function(label){
+load_TWAS_enrichment <- function(){
+    return(data.table::fread("clincial_phenotypes_enrichment_analysis_TWAS.tsv") %>%
+           mutate_at("OR", ~replace(., is.na(.), 1)))
+}
+memTWAS <- memoise::memoise(load_TWAS_enrichment)
+
+gen_data <- function(fnc){
     err = 0.0000001
-    dt <- memENRICH() %>% mutate_if(is.character, as.factor) %>%
-        filter(str_detect(Comparison, label)) %>%
+    dt <- fnc() %>% mutate_if(is.character, as.factor) %>%
         mutate(Tissue=fct_relevel(Tissue, rev), `-log10(FDR)`= -log10(FDR),
                `OR Percentile`= OR / (1+OR), p.fdr.sig=FDR < 0.05,
                `log2(OR)` = log2(OR+err),
@@ -27,29 +32,30 @@ gen_data <- function(label){
 }
 memDF <- memoise::memoise(gen_data)
 
-plot_tile <- function(label){
-    y0 <- min(memDF(label)$`log2(OR)`)-0.1
-    y1 <- max(memDF(label)$`log2(OR)`)+0.1
-    tile_plot <- memDF(label) %>%
-        ggplot(aes(x = Comparison, y = Tissue, fill = `log2(OR)`,
+plot_tile <- function(fnc, label, w, h){
+    y0 <- min(memDF(fnc)$`log2(OR)`)-0.1
+    y1 <- max(memDF(fnc)$`log2(OR)`)+0.1
+    tile_plot <- memDF(fnc) %>%
+        ggplot(aes(x = Dataset, y = Tissue, fill = `log2(OR)`,
                    label = ifelse(p.fdr.sig,
                                   format(round(`-log10(FDR)`,1), nsmall=1), ""))) +
-        ylab('Ancestry-Related DEGs') + xlab('') + facet_grid(.~Direction) +
+        ylab('Ancestry-Related DEGs') + xlab(label) + facet_grid(.~Direction) +
         geom_tile(color = "grey") + ggfittext::geom_fit_text(contrast = TRUE) +
         scale_fill_gradientn(colors=c("blue", "white", "red"),
                              values=scales::rescale(c(y0, 0, y1)),
                              limits=c(y0,y1)) +
-        ggpubr::theme_pubr(base_size = 20, border=TRUE) +
+        ggpubr::theme_pubr(base_size = 20, border=FALSE) +
         theme(axis.text.x = element_text(angle = 45, hjust=1),
               legend.position="right",
               axis.title=element_text(face="bold"),
-              axis.text.y=element_text(face="bold"))
-    save_plot(tile_plot, paste0("tileplot_enrichment_",tolower(label)), 14, 7)
+              axis.text.y=element_text(face="bold"),
+              strip.text = element_text(face="bold"))
+    save_plot(tile_plot, paste0("tileplot_enrichment_",tolower(label)), w, h)
 }
 
-for(label in c("DEG", "TWAS")){
-    plot_tile(label)
-}
+## Run script
+plot_tile(memDEG, "DEGs", 24, 8)
+plot_tile(memTWAS, "TWAS", 16, 6)
 
 ## Reproducibility information
 Sys.time()
