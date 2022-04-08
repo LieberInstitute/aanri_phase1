84699bd91333431048c76955262bc1b5e0bc0f46
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-04-08 10:53:12 -0400

    adding parallel preservation script

---
+++ wgcna_analysis/hippocampus/consensus/parallel_preservation/_h/clusterRepro.R
@@ -0,0 +1,68 @@
+library(foreach)
+library(doParallel)
+
+numCores <- detectCores() - 2
+registerDoParallel(numCores)
+
+permuteCol <- function(x) {
+    n <- dim(x)[2]; p <- dim(x)[1]
+    mm <- runif(length(x)) + rep(seq(n) * 10, rep(p, n))
+    matrix(x[order(mm)], p, n, byrow = FALSE)
+}
+
+IGP.clusterRepro <- function(Data, Centroids) {
+    Result <- c(); Class <- rep(NA, ncol(Data))
+    names(Class) <- colnames(Data); Features <- rownames(Data)
+    Features <- Features[which(Features %in% rownames(Centroids))]
+    Data <- Data[Features,]; Centroids <- Centroids[Features,]
+    Correlation <- cor(Data, Centroids, use = "pairwise.complete.obs", method = "pearson")
+    for (i in 1:nrow(Correlation)) {
+        Class[i] <- which.max(Correlation[i,])
+    }
+    Correlation <- cor(Data, method = "pearson", use = "pairwise.complete.obs")
+    diag(Correlation) <- -Inf
+    Classification <- matrix(0, ncol(Centroids), ncol(Centroids))
+    for (i in 1:nrow(Correlation)) {
+        Classification[Class[i], Class[which.max(Correlation[i,])]] <- Classification[Class[i], Class[which.max(Correlation[i,])]] + 1
+    }
+    Result$Class <- Class
+    Result$IGP <- diag(Classification)/rowSums(Classification)
+    Result$Size <- rowSums(Classification)
+  return(Result)
+}
+
+clusterRepro <- function(Centroids, New.data, Number.of.permutations) {
+    Result <- c(); Features <- rownames(New.data)
+    Features <- Features[which(Features %in% rownames(Centroids))]
+    New.data <- New.data[Features,]
+    Centroids <- Centroids[Features,]
+    Calculation <- IGP.clusterRepro(New.data, Centroids)
+    Actual.IGP <- Calculation$IGP
+    Actual.Size <- Calculation$Size
+    IGP <- c(); Size <- c(); SVD <- svd(Centroids)
+    Centroids.prime <- Centroids%*%SVD$v
+    df <- foreach (i=1:Number.of.permutations, .combine=rbind) %dopar% {
+        New.centroids.prime <- permuteCol(Centroids.prime)
+        New.centroids <- New.centroids.prime %*% t(SVD$v)
+        rownames(New.centroids) <- rownames(Centroids)
+        Calculation <- IGP.clusterRepro(New.data, New.centroids)
+        IGP <- c(IGP, Calculation$IGP)
+        Size <- c(Size, Calculation$Size)
+        data.frame("IGP"=IGP, "Size"=Size)
+    }
+    Result$p.value <- c(NA, length(Actual.Size))
+    Result$Number <- c(NA, length(Actual.Size))
+    for (i in 1:length(Actual.Size)) {
+        if (length(which(df$Size == Actual.Size[i])) == 0 || Actual.Size[i] == 0) {
+            Result$p.value[i] <- NA;
+            Result$Number[i]  <- 0;
+        } else {
+            Same.size         <- df$IGP[which(df$Size == Actual.Size[i])];
+            Result$p.value[i] <- length(which(Same.size >= Actual.IGP[i]))/length(Same.size);
+            Result$Number[i]  <- length(Same.size);
+        }
+    }
+    Result$Actual.IGP <- Actual.IGP
+    Result$Actual.Size <- Actual.Size
+    return(Result)
+}
