3516db14f30da43641dd67dd3083e4d00b6e5a97
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-07-19 19:44:50 -0400

    adding scripts for celltype enrichment

---
+++ differential_analysis/tissue_comparison/celltype_enrichment/_h/celltype_enrichment.py
@@ -0,0 +1,97 @@
+## Cell type enrichment analysis
+
+import numpy as np
+import pandas as pd
+from functools import lru_cache
+from scipy.stats import fisher_exact
+from statsmodels.stats.multitest import fdrcorrection
+
+@lru_cache()
+def get_celltype_genes():
+    fn = '../../../../input/celltypes/_m/Zeisel_single_cell.tsv'
+    return pd.read_csv(fn, sep='\t', index_col=0)
+
+
+@lru_cache()
+def get_annotation():
+    base_loc = "/dcs04/lieber/statsgen/jbenjami/projects/aanri_phase1/input/text_files_counts/"
+    config = {
+        "genes": "%s/_m/caudate/gene_annotation.tsv" % base_loc,
+        "transcripts": "%s/_m/caudate/tx_annotation.tsv" % base_loc,
+        "exons": "%s/_m/caudate/exon_annotation.tsv" % base_loc,
+        "junctions": "%s/_m/caudate/jxn_annotation.tsv" % base_loc,
+    }
+    return pd.read_csv(config["genes"], sep='\t')\
+             .loc[:, ["names", "seqnames", "start", "end", "Symbol", "gencodeID"]]
+
+
+@lru_cache()
+def get_mash_lfsr(tissue):
+    # get lfsr
+    return pd.read_csv("../../_m/genes/lfsr_feature_4tissues.txt.gz", sep='\t')\
+             .loc[:, ["Effect", tissue]]\
+             .rename(columns={tissue: "lfsr"})
+
+
+@lru_cache()
+def get_mash_es(tissue):
+    # get effect size
+    return pd.read_csv("../../_m/genes/posterior_mean_feature_4tissues.txt.gz", sep='\t')\
+             .loc[:, ["Effect", tissue]]\
+             .rename(columns={tissue: "posterior_mean"})
+
+
+@lru_cache()
+def annotate_mash(tissue):
+    return get_mash_lfsr(tissue)\
+        .merge(get_mash_es(tissue), on="Effect")\
+        .merge(get_annotation(), left_on="Effect", right_on="names")\
+        .drop(["names"], axis=1).dropna(subset=["Symbol"])
+
+
+@lru_cache()
+def merge_dataframe(celltype, tissue):
+    return annotate_mash(tissue).merge(get_celltype_genes().loc[:, [celltype]],
+                                       left_on='Symbol', right_index=True)
+
+
+def cal_fishers_direction(celltype, direction, tissue):
+    df = merge_dataframe(celltype, tissue)
+    if direction == 'Up':
+        df = df[(df['posterior_mean'] > 0)].copy()
+    elif direction == 'Down':
+        df = df[(df['posterior_mean'] < 0)].copy()
+    else:
+        df = df
+    
+    table = [[np.sum((df['lfsr']<0.05) & (df[celltype] == 1)), 
+              np.sum((df['lfsr']<0.05) & (df[celltype] == 0))],
+             [np.sum((df['lfsr']>0.05) & (df[celltype] == 1)), 
+              np.sum((df['lfsr']>0.05) & (df[celltype] == 0))]]
+    return fisher_exact(table)
+
+
+def calculate_enrichment():
+    region_lt = []; dir_lt = []; fdr_lt = []; ct_lt = []; pval_lt = []; oddratio_lt = []
+    for tissue in ["Caudate", "Dentate.Gyrus", "DLPFC", "Hippocampus"]:
+        pvals = []
+        for direction in ['Up', 'Down', 'All']:
+            for celltype in get_celltype_genes().columns.unique():
+                odd_ratio, pval = cal_fishers_direction(celltype, direction, tissue)
+                ct_lt.append(celltype); pvals.append(pval); region_lt.append(tissue)
+                oddratio_lt.append(odd_ratio); dir_lt.append(direction)
+        _, fdr = fdrcorrection(pvals) # FDR correction per comparison and version
+        pval_lt = np.concatenate((pval_lt, pvals))
+        fdr_lt = np.concatenate((fdr_lt, fdr))
+    # Generate dataframe
+    return pd.DataFrame({'Tissue': region_lt, 'Cell_type': ct_lt, 'OR': oddratio_lt,
+                         'PValue': pval_lt, "FDR": fdr_lt, 'Direction': dir_lt})
+
+
+def main():
+    calculate_enrichment().to_csv('celltype_enrichment_analysis.txt', sep='\t', index=False)
+
+
+if __name__ == "__main__":
+    main()
+    
