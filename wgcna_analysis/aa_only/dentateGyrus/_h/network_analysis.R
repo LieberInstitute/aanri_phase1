26575118750709fac24aefef17bbdeb8f14daf36
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-10-15 17:43:14 -0400

    updating dentate gyrus scripts, co-expression network analysis

--- wgcna_analysis/aa_only/dentateGyrus/_h/network_analysis.R
+++ wgcna_analysis/aa_only/dentateGyrus/_h/network_analysis.R
@@ -6,145 +6,29 @@ suppressMessages({
 
 PARAM_NETWORK_TYPE = 'signed'
 options(stringsAsFactors = FALSE)
-doParallel::registerDoParallel(cores=30)
-
-filter_outliers <- function(expression, z_threshold = 2.5){
-                                        # Input: an expression matrix
-                                        # Output: an expression matrix with
-                                        # outliers removed.
-    sample_distance = dist(expression)
-    dist_z = scale(colSums(as.matrix(sample_distance)))
-    stopifnot(all(rownames(dist_z) == rownames(expression)))
-    keepSamples = dist_z < z_threshold
-    new_expression = expression[keepSamples,]
-    new_expression
-}
-
-prepare_data <- function(){
-                                        # Load sample data
-    load("../../../../aa_replication_de/dentateGyrus/_m/genes/voomSVA.RData")
-    sample_table <- v$design %>% as.data.frame %>% select(-Intercept) %>%
-        rename("Ancestry"="EA", "Sex"="Male")
-                                        # Load residualized expression
-    vsd <- data.table::fread(paste0("../../../../aa_replication_de/dentateGyrus/",
-                                    "_m/genes/residualized_expression.tsv")) %>%
-        replace(is.na(.), "") %>% tibble::column_to_rownames("V1")
-    print(dim(vsd))
-                                        # Keep only the columns and rows that
-                                        # are present in both the sample table
-                                        # and vsd file
-    samples      <- intersect(colnames(vsd), rownames(sample_table))
-    vsd          <- vsd[,samples]
-    sample_table <- sample_table[samples,]
-                                        # WGCNA data import
-    datExpr0 <- t(vsd)
-                                        # Remove offending genes and samples
-                                        # from the data
-    gsg <- goodSamplesGenes(datExpr0, verbose = 3);
-    if (!gsg$allOK){
-        datExpr0 <- datExpr0[gsg$goodSamples, gsg$goodGenes]
-    }
-    datExpr <- datExpr0
-                                        # Remove outliers
-    datExpr <- filter_outliers(datExpr0, z_threshold = 2.5)
-    rm(datExpr0)
-                                        # Clean data
-    samples      <- intersect(rownames(datExpr), rownames(sample_table))
-    sample_table <- sample_table[samples,]
-    datExpr      <- datExpr[samples,]
-    print(dim(datExpr))
-    save(datExpr, sample_table, file = '00.RData')
-}
-
-
-prepare_traits <- function(){
-    lnames    <- load('00.RData')
-                                        # Associate traits with samples
-    traitRows <- match(rownames(datExpr), rownames(sample_table))
-    datTraits <- sample_table[traitRows,]
-                                        # Diagnostic plot: Sample dendrogram and
-                                        # trait heatmap
-    pdf(file='sample_dendrogram_and_trait_heatmap.pdf', height=16, width = 22)
-    sampleTree2 <- hclust(dist(datExpr), method = "average")
-                                        # Convert traits to a color
-                                        # representation: white means low, red
-                                        # means high, grey means missing entry
-    traitColors <- numbers2colors(traitRows, signed=FALSE);
-                                        # Plot the sample dendrogram and the
-                                        # colors underneath.
-    plotDendroAndColors(sampleTree2, traitColors, groupLabels="Avg. Counts",
-                        main = "Sample dendrogram and trait heatmap",
-                        cex.dendroLabels=0.7)
-    dev.off()
-    save(datExpr, sample_table, datTraits, file = "01.RData")
-}
-
-plot_power_parameter <- function(datExpr, plot_filename){
-                                        # Choose a set of soft-thresholding
-                                        # powers
-    powers <- c(seq(1, 10, 1),  seq(12, to=20, by=2))
-                                        # Call the network topology analysis
-                                        # function
-    sft = pickSoftThreshold(datExpr, networkType = PARAM_NETWORK_TYPE,
-                            powerVector = powers, verbose = 5)
-                                        # Plot the results:
-    pdf(file=plot_filename)
-    par(mfcol = c(2,2)); par(mar = c(4.2, 4.5 , 2.2, 0.5),oma=c(0,0,2,0))
-    cex1 = 0.7;
-                                        # Scale-free topology fit index as a
-                                        # function of the soft-thresholding power
-    plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
-         xlab="Soft Threshold (power)",
-         ylab="Scale Free Topology Model Fit,signed R^2",type="n",
-         main = paste("Scale independence"))
-    text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
-         labels=powers,cex=cex1,col="blue");
-                                        # this line corresponds to using an R^2
-                                        # cut-off of h
-    abline(h=0.80,col="red")
-                                        # Mean connectivity as a function of the
-                                        # soft-thresholding power
-    plot(sft$fitIndices[,1], sft$fitIndices[,5],
-         xlab="Soft Threshold (power)", ylab="Mean Connectivity",
-         type="n", main = paste("Mean connectivity"))
-    text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="blue")
-    #####
-    plot(sft$fitIndices[,1], sft$fitIndices[,6],
-         xlab="Soft Threshold (power)", ylab="Median Connectivity",
-         type="n", main = paste("Median connectivity"))
-    text(sft$fitIndices[,1], sft$fitIndices[,6], labels=powers, cex=cex1,col="blue")
-    #####
-    plot(sft$fitIndices[,1], sft$fitIndices[,7],
-         xlab="Soft Threshold (power)", ylab="Max Connectivity",
-         type="n", main = paste("Max connectivity"))
-    text(sft$fitIndices[,1], sft$fitIndices[,7], labels=powers, cex=cex1,col="blue")
-    dev.off()
-}
-
-figure_out_power_parameter <- function(){
-    lnames <- load(file = '01.RData')
-    plot_power_parameter(datExpr, 'power_parameter_selection.pdf')
-}
+doParallel::registerDoParallel(cores=15)
 
 construct_network <- function(softPower){
-  lnames <- load(file = "01.RData")
-  cor <- WGCNA::cor
-  net = blockwiseModules(datExpr, #mergeCutHeight = 0.2,
-                         power = softPower,
-                         networkType = PARAM_NETWORK_TYPE,
-                         TOMType = PARAM_NETWORK_TYPE,
-                         numericLabels = TRUE, corType = "bicor",
-                         saveTOMs = TRUE, saveTOMFileBase = "TOM",
-                         verbose = 3, maxBlockSize=30000)
-  moduleLabels = net$colors
-  moduleColors = labels2colors(net$colors)
-  MEs = net$MEs;
-  geneTree = net$dendrograms[[1]];
-  save(net, MEs, moduleLabels, moduleColors, geneTree, softPower, file="02.RData")
+    load(file = "01.RData", verbose=TRUE)
+    enableWGCNAThreads(nThreads = 15)
+    cor <- WGCNA::cor
+    net = blockwiseModules(datExpr, mergeCutHeight = 0.3,
+                           power = softPower, minModuleSize = 30,
+                           networkType = PARAM_NETWORK_TYPE,
+                           TOMType = PARAM_NETWORK_TYPE,
+                           numericLabels = TRUE, corType = "bicor",
+                           saveTOMs = TRUE, saveTOMFileBase = "TOM",
+                           verbose = 3, maxBlockSize=30000)
+    moduleLabels = net$colors
+    moduleColors = labels2colors(net$colors)
+    MEs = net$MEs;
+    geneTree = net$dendrograms[[1]];
+    save(net, MEs, moduleLabels, moduleColors, geneTree,
+         softPower, file="02.RData")
 }
 
 plot_cluster_dendrogram <- function(){
-    load(file = "02.RData")
+    load(file = "02.RData", verbose=TRUE)
     pdf(file="cluster_dendrogram.pdf", height=16, width=22)
     mergedColors = labels2colors(net$colors)
     plotDendroAndColors(net$dendrograms[[1]], mergedColors[net$blockGenes[[1]]],
@@ -154,8 +38,8 @@ plot_cluster_dendrogram <- function(){
 }
 
 correlate_with_traits <- function(){
-    lnames <- load(file = "01.RData")
-    lnames <- load(file = "02.RData")
+    load(file = "01.RData", verbose=TRUE)
+    load(file = "02.RData", verbose=TRUE)
                                         # Define numbers of genes and samples
     nGenes = ncol(datExpr); nSamples = nrow(datExpr);
                                         # Recalculate MEs with color labels
@@ -183,8 +67,8 @@ correlate_with_traits <- function(){
 }
 
 export_eigengene_tables <- function(){
-    lnames <- load(file = "01.RData")
-    lnames <- load(file = "02.RData")
+    load(file = "01.RData", verbose=TRUE)
+    load(file = "02.RData", verbose=TRUE)
                                         # Define numbers of genes and samples
     nGenes   <- ncol(datExpr)
     nSamples <- nrow(datExpr)
@@ -199,12 +83,7 @@ export_eigengene_tables <- function(){
 }
 
 #### MAIN
-prepare_data()
-                                        # Sample dendrogram and trait heatmap
-prepare_traits()
-                                        # Scale free topology model fit
-figure_out_power_parameter()
-                                        # SoftPower based on Dentate Gyrus
+                                        # Softpower based on Dentate Gyrus
 construct_network(softPower=12)
                                         # TOM dendrogram
 plot_cluster_dendrogram()
