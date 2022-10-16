2e80ed0d457c2e19af9905fd37d6ebd2a737ce81
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-10-15 20:34:14 -0400

    updated scripts for qSV exploration

--- cell_deconvolution/qsv_heatmap/_h/deconvolution_plots.R
+++ cell_deconvolution/qsv_heatmap/_h/deconvolution_plots.R
@@ -1,36 +1,53 @@
 ## Author: Louise Huuki
 ## Edited by KJ Benjamin
 
-library(SummarizedExperiment)
-library(DeconvoBuddies)
-library(RColorBrewer)
-library(sessioninfo)
-library(patchwork)
-library(tidyverse)
-library(viridis)
-library(broom)
+suppressPackageStartupMessages({
+    library(sessioninfo)
+    library(tidyverse)
+    library(viridis)
+    library(broom)
+    library(here)
+})
 
 ## Functions
 get_qsv_file <- function(tissue){
-    qsv_lt = list("Caudate"="../../../input/phenotypes/_m/qSV_caudate.csv",
-                  "DLPFC"="../../../input/phenotypes/_m/qSV_dlpfc.csv",
-                  "Hippocampus"="../../../input/phenotypes/_m/qSV_hippo.csv",
-                  "Dentate Gyrus"="../../../input/phenotypes/_m/qSV_dg.csv")
+    qsv_lt = list("Caudate"=here("input/phenotypes/_m/qSV_caudate.csv"),
+                  "DLPFC"=here("input/phenotypes/_m/qSV_dlpfc.csv"),
+                  "Hippocampus"=here("input/phenotypes/_m/qSV_hippo.csv"),
+                  "Dentate Gyrus"=here("input/phenotypes/_m/qSV_dg.csv"))
     return(qsv_lt[[tissue]])
 }
 
 save_img <- function(image, fn, w=7, h=7){
-    for(ext in c(".svg", ".pdf", ".png")){
+    for(ext in c(".svg", ".pdf")){
         ggsave(file=paste0(fn, ext), plot=image, width=w, height=h)
     }
 }
 
-get_pheno <- function(){
-    fname = "../../../input/phenotypes/merged/_m/merged_phenotypes.csv"
-    df = data.table::fread(fname) %>% select(-V1) %>%
-        filter(Dx %in% c("Control"), Age > 17, Race %in% c("AA", "CAUC")) %>%
-        mutate(Race = gsub("CAUC", "EA", Race))
-    return(df)
+tissue_map <- function(region){
+    return(list("Caudate"="caudate", "Dentate Gyrus"="dentateGyrus",
+                "DLPFC"="dlpfc", "Hippocampus"="hippocampus")[[region]])
+}
+
+get_pheno <- function(region){
+    baseloc <- here("differential_analysis/")
+    ## Load voom normalized data
+    load(paste0(baseloc, region, "/_m/genes/voomSVA.RData"))
+    dfx = v$design %>% as.data.frame %>% select(starts_with("qPC")) %>%
+        rownames_to_column("RNum") %>%
+        pivot_longer(!RNum, names_to="Covariate", values_to="Variable")
+    dfx$Covariate <- factor(dfx$Covariate, levels = paste0('qPC', 1:20))
+    dfy = v$targets %>% as.data.frame %>%
+        mutate(concordMapRate=mapply(function(r, n) {sum(r*n)/sum(n)},
+                                     concordMapRate, numMapped)) %>%
+        mutate(across(where(is.character), as.factor)) %>%
+        select(Afr,Race,Sex,Age,RIN,mitoRate,rRNA_rate,
+               totalAssignedGene, overallMapRate, concordMapRate)
+    rownames(dfy) <- rownames(v$targets)
+    dfy = dfy %>% rownames_to_column("RNum") %>%
+        pivot_longer(!RNum, names_to="Covariate", values_to="Variable",
+                     values_transform = list(Variable = as.numeric))
+    return(bind_rows(dfy, dfx) %>% mutate_if(is.character, as.factor))
 }
 memPHENO <- memoise::memoise(get_pheno)
 
@@ -38,47 +55,54 @@ get_cell_prop <- function(){
     ## Load Bisque Estimated Props
     load("../../_m/est_prop_Bisque.v2.Rdata")
     cc = est_prop_bisque$caudate$Est.prop.long %>%
-        inner_join(memPHENO(), by=c("sample"="RNum")) %>%
+        inner_join(memPHENO("caudate"), by=c("sample"="RNum")) %>%
         mutate(Tissue="Caudate")
     dd = est_prop_bisque$dlpfc$Est.prop.long %>%
-        inner_join(memPHENO(), by=c("sample"="RNum")) %>%
+        inner_join(memPHENO("dlpfc"), by=c("sample"="RNum")) %>%
         mutate(Tissue="DLPFC")
     hh = est_prop_bisque$hippo$Est.prop.long %>%
-        inner_join(memPHENO(), by=c("sample"="RNum")) %>%
+        inner_join(memPHENO("hippocampus"), by=c("sample"="RNum")) %>%
         mutate(Tissue="Hippocampus")
     gg = est_prop_bisque$dg$Est.prop.long %>%
         separate(sample, c("sample", "batch")) %>%
-        inner_join(memPHENO(), by=c("sample"="RNum")) %>%
-        mutate(Tissue="Dentate Gyrus")
+        inner_join(memPHENO("dentateGyrus"), by=c("sample"="RNum")) %>%
+        mutate(Tissue="Dentate Gyrus") %>% select(-batch)
     df = bind_rows(cc, dd, hh, gg)
     return(df)
 }
 memPROP <- memoise::memoise(get_cell_prop)
 
 #### Load colors and plotting functions ####
-cell_colors <- create_cell_colors(pallet = "classic")
+cell_types = c("Astro", "Endo", "Micro", "Mural", "Oligo",
+               "OPC", "Tcell", "Excit", "Inhib")
+cell_colors <- DeconvoBuddies::create_cell_colors(cell_types=cell_types,pallet = "classic")
 cell_colors <- cell_colors[c("Astro", "Endo", "Micro", "Mural", "Oligo",
                              "OPC", "Tcell", "Excit", "Inhib")]
 #### Comparison plots ####
-for(tissue in c("Caudate", "DLPFC", "Hippocampus", "Dentate Gyrus")){
-    dirname = gsub(" ", "_", tolower(tissue))
-    dir.create(dirname)
-    ## Boxplot
-    group_boxPlot <- memPROP() %>% filter(Tissue == tissue) %>%
-        ggplot(aes(x = cell_type, y = prop, fill = cell_type)) +
-        geom_boxplot() + labs(x = "Cell Type", y = "Proportion") +
-        ggpubr::theme_pubr(base_size = 15) +
-        scale_fill_manual(values = cell_colors, guide = "none")
-    ## Save image
-    save_img(group_boxPlot, paste0(dirname,"/cellType_boxplots"), h=6)
-    ## Composition barplot
-    prop_df = memPROP() %>% filter(Tissue == tissue)
-    comp_barplot <- plot_composition_bar(prop_df,x_col="Race",sample_col="sample") +
-        ggpubr::theme_pubr(base_size = 15) +
-        scale_fill_manual(values = cell_colors)
-    ## Save image
-    save_img(comp_barplot, paste0(dirname, "/composition_barplot"))
-}
+                                        # Boxplot
+group_boxPlot <- memPROP() %>% #filter(Tissue == tissue) %>%
+    ggplot(aes(x = cell_type, y = prop, fill = cell_type)) +
+    geom_boxplot() + labs(x = "Cell Type", y = "Proportion") +
+    facet_wrap("~Tissue") + ggpubr::theme_pubr(base_size = 15) +
+    scale_fill_manual(values = cell_colors, guide = "none") +
+    theme(strip.text = element_text(face="bold"),
+          axis.text.x = element_text(angle=45,hjust=1),
+          axis.title = element_text(face="bold"))
+                                        # Save image
+save_img(group_boxPlot, paste0("cellType_boxplots_4regions"), h=9, w=9)
+
+##                                         # Composition barplot
+## for(tissue in c("Caudate", "DLPFC", "Hippocampus", "Dentate Gyrus")){
+##     dirname = gsub(" ", "_", tolower(tissue))
+##     dir.create(dirname)
+##     prop_df = memPROP() %>% filter(Tissue == tissue)
+##     comp_barplot <- DeconvoBuddies::plot_composition_bar(prop_df,#x_col="Race",
+##                                                          sample_col="sample") +
+##         ggpubr::theme_pubr(base_size = 15) +
+##         scale_fill_manual(values = cell_colors)
+##                                         # Save image
+##     save_img(comp_barplot, paste0(dirname, "/composition_barplot"))
+## }
 
 #### Correlation with qSV ####
 for(tissue in c("Caudate", "DLPFC", "Hippocampus", "Dentate Gyrus")){
@@ -89,7 +113,7 @@ for(tissue in c("Caudate", "DLPFC", "Hippocampus", "Dentate Gyrus")){
     ## Bind with qSV table
     est_prop_qsv <- memPROP() %>% filter(Tissue == tissue) %>%
         mutate(RNum = sample) %>% left_join(qSV_long, by = "RNum")
-    est_prop_qsv$qSV <- factor(est_prop_qsv$qSV, levels = paste0('PC', 1:13))
+    est_prop_qsv$qSV <- factor(est_prop_qsv$qSV, levels = paste0('PC', 1:15))
     ## Calculate p-values ##
     prop_qSV_fit <- est_prop_qsv %>% group_by(cell_type, qSV) %>%
         do(fitQSV = tidy(lm(prop ~ qSV_value, data = .))) %>%
@@ -105,18 +129,19 @@ for(tissue in c("Caudate", "DLPFC", "Hippocampus", "Dentate Gyrus")){
     print(prop_qSV_fit %>% count(p.bonf.cat))
     ## Tile plots
     my_breaks <- c(0.05, 0.01, 0.005, 0)
-    tile_plot_val <- prop_qSV_fit %>%
+    tile_plot_val <- prop_qSV_fit %>% mutate(Tissue = tissue) %>%
         ggplot(aes(x = cell_type, y = qSV, fill = log.p.bonf,
                    label=ifelse(p.bonf.sig,
                                 format(round(log.p.bonf,1), nsmall=1), ""))) +
         geom_tile(color = "grey") + ggfittext::geom_fit_text(contrast = TRUE) +
-        scale_color_viridis(option = "magma") +
+        facet_wrap("~Tissue") + scale_color_viridis(option = "magma") +
         scale_fill_viridis(name="-log10(p-value Bonf)", option="magma",
                            direction=-1, limits=c(0,50)) +
-        labs(title ="p-values cell-type prop~qSV", x = 'Cell Type',
-             color ="p-value Bonf\nsignificance") +
-        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-        ggpubr::theme_pubr(base_size = 15)
+        labs(x = 'Cell Type', color ="p-value Bonf\nsignificance") +
+        ggpubr::theme_pubr(base_size = 15) +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+              axis.title = element_text(face="bold", size=18),
+              strip.text = element_text(face="bold"))
     save_img(tile_plot_val, paste0(dirname, "/qSV_prop_fit_tileVal"))
     ## Create scatter plots
     sig_colors2 <- c("#440154","#31688E", "#35B779","black")
