76ba6b5f4c90893c26b7424106054881bfa2d071
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-11-09 14:12:17 -0500

    adding script for mash modeling for ancestry effects

---
+++ genetic_variation/predict_expression/tissue_comparison/_h/step2c.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#$ -cwd
+#$ -R y
+#$ -l mem_free=15.0G,h_vmem=15G,h_fsize=50G
+#$ -N run_mash
+#$ -o ./mash.out
+#$ -e ./mash.out
+#$ -m e -M jade.benjamin@libd.org
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+## List current modules for reproducibility
+module load R/3.6.1
+module list
+## Edit with your job command
+FEATURE="exons"
+
+echo "**** Run mash model ****"
+Rscript ../_h/mashr_script.R --feature $FEATURE
+
+echo "**** Job ends ****"
+date
