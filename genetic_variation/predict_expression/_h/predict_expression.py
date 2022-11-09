f9db4700be23cf14d70085abaf6c6eb4bfe88042
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-11-09 14:11:07 -0500

    adding scripts for predicted expression, residuals

---
+++ genetic_variation/predict_expression/_h/predict_expression.py
@@ -0,0 +1,149 @@
+"""
+This script runs tensorQTL in python.
+"""
+import pandas as pd
+from pyhere import here
+from functools import lru_cache
+import argparse, torch, session_info
+from tensorqtl import read_phenotype_bed, genotypeio, Residualizer
+
+def map_tissue(tissue):
+    return {"caudate": "Caudate", "dlpfc": "DLPFC",
+            "dentateGyrus": "Dentate_Gyrus",
+            "hippocampus": "Hippocampus"}[tissue]
+
+
+def map_feature(feature):
+    return {"genes": "Gene", "transcripts": "Transcript",
+            "exons": "Exon", "junctions": "Junction"}[feature]
+
+
+def get_phenotype(tissue, feature):
+    expr_bed = here(f"eqtl_analysis/{tissue}/{feature}/",
+                    "normalize_expression/_m/",
+                    f"{feature}.expression.bed.gz")
+    return read_phenotype_bed(str(expr_bed))
+
+
+@lru_cache()
+def get_residualized(tissue, feature):
+    # Load residualized data
+    res_file = here(f"eqtl_analysis/{tissue}/{feature}/covariates/",
+                    "residualized_expression/_m",
+                    f"{feature}_residualized_expression.csv")
+    return pd.read_csv(res_file, index_col=0)
+
+
+@lru_cache()
+def load_de(tissue, feature):
+    de_file = here("differential_analysis/tissue_comparison/summary_table/_m/",
+                   "BrainSeq_ancestry_4features_4regions_allFeatures.txt.gz")
+    de_df = pd.read_csv(de_file, sep='\t')
+    return de_df[(de_df["Tissue"] == map_tissue(tissue).replace("_", " ")) &
+                 (de_df["Type"] == map_feature(feature))].copy()
+
+
+@lru_cache()
+def subset_phenotypes(tissue, feature):
+    phenotype_df, phenotype_pos_df = get_phenotype(tissue, feature)
+    res_df = get_residualized(tissue, feature)
+    # shared_features = set(load_de(tissue, feature).Effect) & \
+    #     set(res_df.index)
+    # res_df = res_df.loc[shared_features].sort_index()
+    return res_df.sort_index()
+
+
+@lru_cache()
+def get_effectsize(tissue, feature):
+    es_file = here(f"eqtl_analysis/tissue_comparison/{feature}/_m/",
+                   "posterior_mean_allpairs_ancestry.txt.gz")
+    return pd.read_csv(es_file, sep='\t')\
+           .loc[:, ["effect", "gene_id", "variant_id", map_tissue(tissue)]]
+
+
+# @lru_cache()
+# def subset_es(tissue, feature):
+#     DEG = set(load_de(tissue, feature).Effect)
+#     es_df = get_effectsize(tissue, feature)
+#     return es_df[(es_df["gene_id"].isin(DEG))].copy()
+
+
+@lru_cache()
+def load_eFeatures(tissue, feature):
+    egene_file = here(f"eqtl_analysis/{tissue}/{feature}/",
+                      "cis_analysis/_m/LIBD_TOPMed_AA.genes.txt.gz")
+    cond_file = here(f"eqtl_analysis/{tissue}/{feature}/",
+                     "cis_analysis/_m/LIBD_TOPMed_AA.conditional.txt.gz")
+    cols = ["phenotype_id","variant_id","slope","slope_se","pval_nominal","qval"]
+    eGenes = pd.read_csv(egene_file, sep='\t').loc[:, cols]
+    conditional = pd.read_csv(cond_file, sep='\t')
+    conditional["qval"] = 0.05
+    return pd.concat([eGenes, conditional.loc[:, cols]], axis=0)
+
+
+@lru_cache()
+def get_genotypes(tissue, feature):
+    plink_prefix_path = here(f"eqtl_analysis/{tissue}/{feature}/",
+                             "plink_format/_m/genotypes")
+    pr = genotypeio.PlinkReader(str(plink_prefix_path))
+    variant_df = pr.bim.set_index("snp")[["chrom", "pos"]]
+    variant_df.loc[:, "chrom"] = "chr" + variant_df.chrom
+    return pr.load_genotypes(fam_id="fid"), variant_df
+
+
+@lru_cache()
+def subset_genotypes(tissue, feature):
+    genotype_df, variant_df = get_genotypes(tissue, feature)
+    return genotype_df.loc[load_eFeatures(tissue, feature).variant_id]
+
+
+@lru_cache()
+def get_topQTL(tissue, feature):
+    es_df = get_effectsize(tissue, feature)
+    genotype_df = subset_genotypes(tissue, feature)
+    phenotype_df = subset_phenotypes(tissue, feature)
+    eFeatures = load_eFeatures(tissue, feature)\
+        .loc[:, ["phenotype_id", "variant_id", "qval"]]\
+        .merge(es_df, left_on=["phenotype_id", "variant_id"],
+               right_on=["gene_id", "variant_id"])\
+        .drop(["gene_id", "effect"], axis=1)\
+        .rename(columns={map_tissue(tissue): "posterior_mean"})\
+        .set_index("phenotype_id")
+    shared_features = list(set(phenotype_df.index) & set(eFeatures.index))
+    top_feature = eFeatures.sort_values("qval").groupby(["phenotype_id"])\
+                           .first().loc[shared_features, ["variant_id","qval",
+                                                          "posterior_mean"]]
+    return pd.merge(top_feature, genotype_df, left_on="variant_id",
+                    right_index=True).reset_index().drop_duplicates()
+
+
+def get_predExpr_topQTL(tissue, feature):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    beta = get_topQTL(tissue, feature).set_index("phenotype_id")\
+                                      .loc[:, ["posterior_mean"]]
+    top_df = get_topQTL(tissue, feature).set_index("phenotype_id")\
+                                        .drop(["variant_id","qval","posterior_mean"],
+                                              axis=1)
+    genotypes_df = torch.tensor(top_df.values, dtype=torch.float32).to(device)
+    beta_df = torch.tensor(beta.values, dtype=torch.float32).to(device)
+    # Predict expression
+    return pd.DataFrame(torch.mul(beta_df, genotypes_df).cpu(),
+                        columns=top_df.columns, index=top_df.index).astype("float")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tissue', type=str)
+    parser.add_argument('--feature', type=str)
+    args=parser.parse_args()
+
+    # Extract predicted expression (Effect Size X Dosage [0|1|2])
+    tissue = args.tissue; feature = args.feature
+    get_predExpr_topQTL(tissue, feature)\
+        .to_csv(f"{feature}.predicted_expression.{tissue}.txt.gz", sep='\t')
+    ## Session information
+    session_info.show()
+    
+
+if __name__ == "__main__":
+    main()
---
+++ genetic_variation/predict_expression/_h/step_1a.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#$ -cwd
+#$ -l caracol,mem_free=40G,h_vmem=40G,h_fsize=100G
+#$ -N predExpr_gpu_caudate_gene
+#$ -o ./caudate_summary.log
+#$ -e ./caudate_summary.log
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+## List current modules for reproducibility
+module load R/3.6.1
+module list
+
+USAGE_CUTOFF=10
+NUM_GPUS=1
+
+avail_gpus=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | \
+                 cut -d " " -f 1 | \
+                 awk -v usage="$USAGE_CUTOFF" '$1 < usage {print NR - 1}')
+
+#  Simply exit with an error if there are no GPUs left
+if [[ -z $avail_gpus ]]; then
+    echo "No GPUs are available."
+    exit 1
+fi
+
+export CUDA_VISIBLE_DEVICES=$(echo "$avail_gpus" | head -n $NUM_GPUS | \
+                                  paste -sd ",")
+
+## Edit with your job command
+TISSUE="caudate"; FEATURE="genes"
+
+echo "**** Run tensorQTL ****"
+python ../_h/predict_expression.py --tissue $TISSUE --feature $FEATURE
+echo "**** Job ends ****"
+date
+
+## This script was edited from the gpu example here:
+## https://gist.github.com/lahuuki/6fa1575bb15d80aeecdcabc5e2ad5f16
