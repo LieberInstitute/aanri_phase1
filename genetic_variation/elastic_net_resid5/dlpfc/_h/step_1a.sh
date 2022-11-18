304c7816bdc839e6ff1e387749bda1c4a1f4fb87
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-11-18 07:02:09 -0500

    adding scripts for DE analysis with elastic net predicted residualized expression

---
+++ genetic_variation/elastic_net_resid5/dlpfc/_h/step_1a.sh
@@ -0,0 +1,27 @@
+#$ -cwd
+#$ -R y
+#$ -l mem_free=5.0G,h_vmem=5G,h_fsize=50G
+#$ -N 'predicted_expr_dlpfc'
+#$ -o ./summary.out
+#$ -e ./summary.out
+#$ -m e -M jade.benjamin@libd.org
+
+echo "**** Job starts ****"
+date -Is
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+module load gcc/9.1.0
+module load pandoc
+module load R
+module list
+
+echo "**** Run sensitivity analysis for DE ****"
+Rscript  ../_h/differential_expression.R --feature "genes"
+
+echo "**** Job ends ****"
+date -Is
---
+++ genetic_variation/elastic_net_resid5/hippocampus/_h/differential_expression.R
@@ -0,0 +1,78 @@
+## Differential expression with limma-voom pipeline
+suppressMessages({
+    library(here)
+    library(argparse)
+    library(tidyverse)
+})
+
+extract_de <- function(contrast, label, efit, feature){
+    top <- limma::topTable(efit, coef=contrast, number=Inf, sort.by="P")
+    top$SE <- sqrt(efit$s2.post) * efit$stdev.unscaled
+    top.fdr <- top %>% filter(adj.P.Val<=0.05)
+    print(paste("Comparison for:", label))
+    print(paste('There are:', dim(top.fdr)[1], 'DE features!'))
+    top %>% rownames_to_column("gene_id") %>%
+        data.table::fwrite(file=paste0(feature, "/diffExpr_", label, "_full.txt"), 
+                           sep='\t', row.names=FALSE)
+    top.fdr %>% rownames_to_column("gene_id") %>%
+        data.table::fwrite(file=paste0(feature, "/diffExpr_",
+                                       label, "_FDR05.txt"), 
+                           sep='\t', row.names=FALSE)
+}
+
+load_model <- function(feature){
+    fn <- here("differential_analysis/hippocampus/_m/",
+               feature,"/voomSVA.RData")
+    load(fn)
+    design0 <- v$targets %>% select(BrNum, RNum)
+    return(v$design %>% as.data.frame %>% rownames_to_column("RNum") %>%
+        inner_join(design0, by="RNum") %>% column_to_rownames("BrNum") %>%
+        select(Intercept, EA) %>% as.matrix)
+}
+
+get_background <- function(feature){
+    fn <- here("differential_analysis/hippocampus/_m/",
+               feature,"/voomSVA.RData")
+    load(fn)
+    return(rownames(v$genes))
+}
+
+fit_model <- function(feature){
+    # Load data and design matrix
+    res_file <- paste0("../../_m/",feature,
+                       ".predicted_expression.hippocampus.txt.gz")
+    res_expr <- data.table::fread(res_file) %>%
+        filter(gene_id %in% get_background(feature)) %>%
+        column_to_rownames("gene_id")
+    mod      <- load_model(feature)
+    # Fit model
+    sample_ids   <- intersect(rownames(mod), colnames(res_expr))
+    logCPM       <- res_expr %>% select(any_of(sample_ids))
+    design       <- mod[sample_ids, ]
+    fit0         <- limma::lmFit(logCPM, design)
+    contr.matrix <- limma::makeContrasts(EAvsAA=EA,levels=colnames(design))
+    fit <- limma::contrasts.fit(fit0, contrasts=contr.matrix)
+    esv <- limma::eBayes(fit)
+    return(esv)
+}
+memFIT <- memoise::memoise(fit_model)
+
+#### MAIN analysis
+                                        # Create parser object
+parser <- ArgumentParser()
+parser$add_argument("-f", "--feature", type="character", default="genes",
+                    help="Feature to be analyzed [default: %default]")
+args <- parser$parse_args()
+                                        # Run analysis
+feature <- args$feature
+dir.create(feature)
+                                        # Fit model and apply eBayes
+efit = memFIT(feature)
+                                        # Save differential expression
+extract_de(1, "EAvsAA", efit, feature)
+
+#### Reproducibility information
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
