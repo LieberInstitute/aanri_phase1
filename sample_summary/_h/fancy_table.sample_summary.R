2f69a949e8dc8b3f4ce099bc04639858843766b2
KrotosBenjamin <kj.benjamin90@gmail.com>
2023-01-23 09:32:41 -0500

    updating names

--- sample_summary/_h/fancy_table.sample_summary.R
+++ sample_summary/_h/fancy_table.sample_summary.R
@@ -1,18 +1,21 @@
 ## Summarize results and plot correlation with q-value
 
 suppressPackageStartupMessages({
+    library(here)
     library(dplyr)
     library(gtsummary)
 })
+options(gt.html_tag_check = FALSE)
 
 save_table <- function(pp, fn){
-    for(ext in c(".pdf", ".tex")){
+    for(ext in c(".pdf", ".tex", ".png")){
         gt::gtsave(as_gt(pp), filename=paste0(fn,ext))
     }
 }
 
 load_phenotypes <- function(){
-    return(data.table::fread("../../input/phenotypes/merged/_m/merged_phenotypes.csv") %>%
+    return(data.table::fread(here("input/phenotypes/merged",
+                                  "_m/merged_phenotypes.csv")) %>%
            mutate_if(is.character, as.factor))
 }
 memPHENO <- memoise::memoise(load_phenotypes)
@@ -25,27 +28,29 @@ pp1 = memPHENO() %>% filter(Race == "AA", Age > 17, Dx == "Control") %>%
     mutate(Sex=factor(Sex, labels=c("Female", "Male")),
            Region=factor(Region, labels=c("Caudate", "Dentate Gyrus",
                                           "DLPFC", "Hippocampus"))) %>%
-    tbl_summary(by="Region", missing="no", #type=all_continuous() ~ "continuous2",
+    tbl_summary(by="Region", missing="no",
+                ##type=all_continuous() ~ "continuous2",
                 statistic = all_continuous() ~ c("{mean} ({sd})")) %>%
     modify_header(all_stat_cols()~"**{level}**<br>N = {n}") %>%
     modify_spanning_header(all_stat_cols()~"**Brain Region**") %>%
     bold_labels() %>% italicize_levels()
 save_table(pp1, fn1)
-    
+
 fn2 = "sample_breakdown.table.all_samples"
-pp2 = memPHENO() %>% filter(Race %in% c("AA", "CAUC"), Age > 17, Dx == "Control") %>%
+pp2 = memPHENO() %>%
+    filter(Race %in% c("AA", "CAUC"), Age > 17, Dx == "Control") %>%
     select(Region, Race, Sex, Age, RIN) %>%
     mutate(Sex=factor(Sex, labels=c("Female", "Male")),
            Region=factor(Region, labels=c("Caudate", "Dentate Gyrus",
                                           "DLPFC", "Hippocampus")),
-           Race=factor(forcats::fct_drop(Race), labels=c("AA", "EA"))) %>%
+           Race=factor(forcats::fct_drop(Race), labels=c("BA","WA"))) %>%
     tbl_strata(
         strata=Region,
         ~.x %>%
             tbl_summary(by="Race", missing="no",
-                        statistic = all_continuous() ~ c("{mean} ({sd})")) %>%
+                        statistic=all_continuous() ~ c("{mean} ({sd})")) %>%
             modify_header(all_stat_cols()~"**{level}**<br>N = {n}") %>%
-            modify_spanning_header(all_stat_cols()~"**Brain Region**") %>%
+            modify_spanning_header(all_stat_cols()~"**Brain Region**")%>%
             bold_labels() %>% italicize_levels()
     )
 save_table(pp2, fn2)
