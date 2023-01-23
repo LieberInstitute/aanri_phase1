2127f1fb70ee55fd98204bf031ac30bd3a7f7dcd
KrotosBenjamin <kj.benjamin90@gmail.com>
2023-01-23 09:39:27 -0500

    updating annotation on Circos plot

--- differential_analysis/tissue_comparison/circos_plot/_h/plot_circos.R
+++ differential_analysis/tissue_comparison/circos_plot/_h/plot_circos.R
@@ -29,9 +29,9 @@ extract_bed <- function(tissue){
 }
 
 plot_circos_4tissue <- function(caudate, dlpfc, hippo, gyrus){
-    lgd_points = Legend(at=c("Increased association with AA", "Decreased association with AA"),
+    lgd_points = Legend(at=c("Increased EA proportion", "Increased AA proportion"),
                         type="points", legend_gp=gpar(col = c("red", "blue")),
-                        title_position="topleft", title="Ancestry Proportion",
+                        title_position="topleft", title="Genetic Ancestry",
                         background="#FFFFFF")
     circos.clear() # clear plot if there is any
     circos.par("start.degree" = 0, "cell.padding" = c(0, 0, 0, 0),
