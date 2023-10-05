92cd6c1f25f03f95a7d084b561ade12707823dfa
KrotosBenjamin <kj.benjamin90@gmail.com>
2023-10-05 09:19:15 -0400

    adding scripts for mash modeling

---
+++ differential_analysis/hla_control/tissue_comparison/_h/step_4.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -cwd
+#$ -R y
+#$ -l mem_free=15.0G,h_vmem=15G,h_fsize=50G
+#$ -N run_mash
+#$ -o ./mash_jxn.out
+#$ -e ./mash_jxn.out
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+## List current modules for reproducibility
+module load gcc/9.1.0
+module load pandoc
+module load conda_R/4.2.x
+
+module list
+
+## Edit with your job command
+FEATURE="junctions"
+
+echo "**** Prepare and run mash modeling ****"
+Rscript ../_h/extract_effectsizes.R --feature $FEATURE
+
+echo "**** Job ends ****"
+date
